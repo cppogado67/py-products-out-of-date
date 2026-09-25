b41546a8627ac9d244e7cee6dccb3130cbf5c824
claro <c_ppogado67@yahoo.com>
2026-09-25 11:45:30 +0800

    0035

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 import datetime
 
 from app.main import outdated_products
@@ -8,21 +8,24 @@ REAL_DATE = datetime.date
 
 
 @patch("datetime.date")
-def test_outdated_products(mock_today):
-	mock_today.today.return_value = REAL_DATE(2022, 2, 5)
-	products = [
-		{"name": "Expired product", "expiration_date": REAL_DATE(2022, 2, 4)},
-		{"name": "Fresh product", "expiration_date": REAL_DATE(2022, 2, 6)},
-	]
+def test_outdated_products(mock_today: MagicMock) -> None:
+    mock_today.today.return_value = REAL_DATE(2022, 2, 5)
+    products = [
+        {"name": "Expired product", "expiration_date": REAL_DATE(2022, 2, 4)},
+        {"name": "Fresh product", "expiration_date": REAL_DATE(2022, 2, 6)},
+    ]
 
-	assert outdated_products(products) == ["Expired product"]
+    assert outdated_products(products) == ["Expired product"]
 
 
 @patch("datetime.date")
-def test_product_expiring_today_is_not_outdated(mock_today):
-	mock_today.today.return_value = REAL_DATE(2022, 2, 5)
-	products = [
-		{"name": "Product expiring today", "expiration_date": REAL_DATE(2022, 2, 5)},
-	]
+def test_product_expiring_today_is_not_outdated(mock_today: MagicMock) -> None:
+    mock_today.today.return_value = REAL_DATE(2022, 2, 5)
+    products = [
+        {
+            "name": "Product expiring today",
+            "expiration_date": REAL_DATE(2022, 2, 5),
+        },
+    ]
 
-	assert outdated_products(products) == []
+    assert outdated_products(products) == []
